196ac80e1376f0599338653ea246944777515fd7
konglx <konglx90@gmail.com>
2020-11-26 08:20:59 +0800

    Updates

---
+++ precache-manifest.96e376d979057418b290c28197a2c2b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55d6fc5cc7b8c23f85d7de8a9ec01d45",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a48bf50665c4fb97f27",
+    "url": "/static/css/main.6bc64233.chunk.css"
+  },
+  {
+    "revision": "dfe2c09dac8cdc53f31b",
+    "url": "/static/js/2.8bdc5915.chunk.js"
+  },
+  {
+    "revision": "76d7a8da27b1246d0ec7305dafc313ce",
+    "url": "/static/js/2.8bdc5915.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3a48bf50665c4fb97f27",
+    "url": "/static/js/main.fe012008.chunk.js"
+  },
+  {
+    "revision": "18aa42414906b91ecba3",
+    "url": "/static/js/runtime-main.b89cff06.js"
+  }
+]);
